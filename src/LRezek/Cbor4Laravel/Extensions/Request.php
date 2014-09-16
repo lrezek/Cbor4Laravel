a52bd54d9dbc0e43e3e84a829489a328d0949237
Lukas Rezek <lukas@miratronix.com>
2014-09-16 09:38:43 -0400

    Update and rename src/LRezek/Cbor4Laravel/Request.php to src/LRezek/Cbor4Laravel/Extensions/Request.php

---
+++ src/LRezek/Cbor4Laravel/Extensions/Request.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LRezek\Cbor4Laravel\Extensions;
+
+class Request extends \Illuminate\Http\Request 
+{
+
+    public function doSomething()
+    {
+        echo 'Doing something!';
+    {
+
+}
+
+?>
