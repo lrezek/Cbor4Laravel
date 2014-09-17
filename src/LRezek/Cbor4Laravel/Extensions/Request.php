685d715f9e80bd5027f058020c7eb5c3cdf0518e
Lukas Rezek <lukas@miratronix.com>
2014-09-17 11:37:05 -0400

    Update Request.php

--- src/LRezek/Cbor4Laravel/Extensions/Request.php
+++ src/LRezek/Cbor4Laravel/Extensions/Request.php
@@ -7,9 +7,53 @@ use \Illuminate\Http\Request as BaseRequest;
 class Request extends BaseRequest
 {
 
-    public function doSomething()
+    protected $cbor;
+
+    /**
+     * Determine if the request is in CBOR.
+     *
+     * @return bool
+     */
+    public function isCbor()
     {
-        echo 'Doing something!';
+         return str_contains($this->header('CONTENT_TYPE'), '/cbor');
+    }
+
+    /**
+     * Determine if the current request is asking for CBOR in return.
+     *
+     * @return bool
+     */
+    public function wantsCbor()
+    {
+        $acceptable = $this->getAcceptableContentTypes();
+ 
+        return isset($acceptable[0]) && ($acceptable[0] == 'application/cbor');
+    }
+    
+    /**
+     * Get the CBOR payload for the request.
+     *
+     * @param  string  $key The key to get the value for, in "dot notation" (ex: user.username = cbor["user"]["username])
+     * @param  mixed   $default Default value if the key isn't found.
+     * @return mixed
+     */
+    public function cbor($key = null, $default = null)
+    {
+        //Don't decode twice!
+        if(!isset($this->cbor))
+        {
+            $this->cbor = new ParameterBag((array) cbor_decode($this->getContent(), true));
+        }
+ 
+        //Return the whole array if no key was specified
+        if(is_null($key))
+        {
+            return $this->cbor;
+        }
+ 
+        //Get key value in dot notation
+        return array_get($this->cbor->all(), $key, $default);
     }
 
 }
